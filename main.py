b6260a0d3c2172b194437678ce96c732cd68f3c7
taczhed <patryklach99@gmail.com>
2025-04-09 12:22:15 +0200

    feat(lab01): Euclidean algorithm (Least common multiple)

---
+++ main.py
@@ -0,0 +1,6 @@
+from labs.lab01 import lcm
+
+print("--- Euclidean algorithm (Least common multiple) ---")
+a = int(input('--- Enter number a: '))
+b = int(input('--- Enter number b: '))
+print("--- Result is: " + str(lcm(a, b)) + " ---")
